85e1b051bf7aa4740010f7ccca9ec892da1750e5
Amit Sahu <sahuami2000@gmail.com>
2024-10-13 16:11:12 +0530

    delete

--- React js/1basic-react/src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-function Title(){
-    return <h1> I am a title</h1>;
-  }
-
-  
-
-  export default Title;
-// export {Title};
--- React js/4moreHook/src/components/SecondUseEffect.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SecondUseEffect() {
-  return (
-    <div>
-      hello baby
-    </div>
-  )
-}
